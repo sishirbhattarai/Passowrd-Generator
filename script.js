9927c1c0333b1aa0fe4e11622a5870e6280b7a8b
Sishir <sishir.bhattarai@outlook.com>
2020-10-02 17:40:10 -0600

    added code

--- script.js
+++ script.js
@@ -13,16 +13,15 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
-  //Arrays
 
+  //Arrays
   var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var lowerCase = "abcdefghijklmnopqrstuvwxyz";
   var numbers = "0123456789";
   var specialCharacter = "!$%&'?()*+,-./:;<=>?@[]^_`{|}";
-  var passwordresult = ""
+  var passwordResult = " ";
 
-  //Prompt and Confirm
-
+  //Prompt and Confirm parameters
   var confirmLength = prompt(
     "What length of passowrd do you need? Please select any length between 8 and 128."
   );
@@ -44,29 +43,70 @@ function generatePassword() {
   }
 
   var confirmLowerCase = confirm("Do you want lowercase in your password?");
+  var confirmUpperCase = confirm("Do you want uppercase in your password?");
+  var confirmNumbers = confirm("Do you want numbers in your password?");
+  var confirmSpecialChar = confirm("Do you want special characters in your password?");
 
-  var confirmUpperCase = confirm("Do you want lowercase in your password?");
+  // If no confirm is made by user, ask again to confirm. while loop if all confirms are false
 
-  var numbers = confirm("Do you want numbers in your password?");
+  while(confirmLowerCase === false && confirmUpperCase === false &&  confirmNumbers === false && confirmSpecialChar === false){
+    alert("Please choose at least one parameter");
+    var confirmLowerCase = confirm("Do you want lowercase in your password?");
+    var confirmUpperCase = confirm("Do you want uppercase in your password?");
+    var confirmNumbers = confirm("Do you want numbers in your password?");
+    var confirmSpecialChar = confirm("Do you want special characters in your password?");
+  }
 
-  var specialChar = confirm("Do you want special characters in your password?");
+  //set of conditions
 
- passwordresult=11 
- return passwordresult
+  if (confirmLowerCase) {
+    passwordResult = passwordResult.concat(lowerCase)
+  }
+
+  if(confirmUpperCase) {
+    passwordResult = passwordResult.concat(upperCase)
+  }
+
+  if(confirmNumbers) {
+    passwordResult = passwordResult.concat(numbers)
+  }
+
+  if(confirmSpecialChar) {
+    passwordResult = passwordResult.concat(specialCharacter)
+  }
+
+console.log(passwordResult);
+
+//Calculation
+
+var finalPassword = "";
+
+  for (i = 0; i < confirmLength; i++) {
+    finalPassword = finalPassword + passwordResult.chartAt(
+      Math.floor(Math.random() * passwordResult.length)
+    );
+  }
+   document.querySelector("Your Secure Password").value = finalPassword
 }
 
 // GIVEN I need a new, secure password
 // WHEN I click the button to generate a password
 // THEN I am presented with a series of prompts for password criteria
+
 // WHEN prompted for password criteria
 // THEN I select which criteria to include in the password
+
 // WHEN prompted for the length of the password
 // THEN I choose a length of at least 8 characters and no more than 128 characters
+
 // WHEN prompted for character types to include in the password
 // THEN I choose lowercase, uppercase, numeric, and/or special characters
+
 // WHEN I answer each prompt
 // THEN my input should be validated and at least one character type should be selected
+
 // WHEN all prompts are answered
 // THEN a password is generated that matches the selected criteria
+
 // WHEN the password is generated
 // THEN the password is either displayed in an alert or written to the page
